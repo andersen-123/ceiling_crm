d17b94afee5ea87904ff7f76446d27268aa577cd
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-21 18:20:27 +0300

    Update estimate.dart

--- lib/models/estimate.dart
+++ lib/models/estimate.dart
@@ -1,31 +1,19 @@
 class Estimate {
-  final int? id;
-  final String clientName;
-  final double area;
-  final double price;
+  int? id;
+  String name;
+  double total;
 
-  Estimate({
-    this.id,
-    required this.clientName,
-    required this.area,
-    required this.price,
-  });
+  Estimate({this.id, required this.name, required this.total});
 
   Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'clientName': clientName,
-      'area': area,
-      'price': price,
-    };
+    return {'id': id, 'name': name, 'total': total};
   }
 
   factory Estimate.fromMap(Map<String, dynamic> map) {
     return Estimate(
       id: map['id'],
-      clientName: map['clientName'],
-      area: map['area'],
-      price: map['price'],
+      name: map['name'],
+      total: map['total'],
     );
   }
 }
