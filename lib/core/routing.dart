84f0596be6604aab2428013790cac9dbee132dfe
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-21 18:13:19 +0300

    Update routing.dart

--- lib/core/routing.dart
+++ lib/core/routing.dart
@@ -1,22 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../screens/home_screen.dart';
-import '../screens/calculator_screen.dart';
 import '../screens/estimate_list_screen.dart';
+import '../screens/calculator_screen.dart';
 
 final router = GoRouter(
-  initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/calculator',
-      builder: (context, state) => const CalculatorScreen(),
-    ),
-    GoRoute(
-      path: '/estimates',
-      builder: (context, state) => const EstimateListScreen(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/estimates', builder: (context, state) => const EstimateListScreen()),
+    GoRoute(path: '/calculator', builder: (context, state) => const CalculatorScreen()),
   ],
 );
