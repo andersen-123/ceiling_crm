977ceb9fb412940eb4936c96719994af04aac6cf
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-22 00:22:49 +0300

    Update project_worker.dart

--- lib/models/project_worker.dart
+++ lib/models/project_worker.dart
@@ -1,1 +1,36 @@
 
+class ProjectWorker {
+  int? id;
+  final int projectId; // ID проекта, к которому привязан
+  final String name; // "Лёша", "Я"
+  final bool hasCar; // true - у работника есть автомобиль
+  double salaryCalculated; // Рассчитанная зарплата (будет обновляться)
+
+  ProjectWorker({
+    this.id,
+    required this.projectId,
+    required this.name,
+    required this.hasCar,
+    this.salaryCalculated = 0.0,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'project_id': projectId,
+      'name': name,
+      'has_car': hasCar ? 1 : 0,
+      'salary_calculated': salaryCalculated,
+    };
+  }
+
+  factory ProjectWorker.fromMap(Map<String, dynamic> map) {
+    return ProjectWorker(
+      id: map['id'],
+      projectId: map['project_id'],
+      name: map['name'],
+      hasCar: map['has_car'] == 1,
+      salaryCalculated: map['salary_calculated'],
+    );
+  }
+}
