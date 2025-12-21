82b02dca9ef12bac115009b97576b3dc4d1317a1
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-21 18:25:09 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'core/routing.dart';
 
 void main() {
-  runApp(const CeilingCRMApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Ceiling CRM',
+      routerConfig: router,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
