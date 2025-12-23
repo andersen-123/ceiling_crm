117f2dc8dfe0cb1a70666389be3b6a84e6468b33
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-24 00:41:30 +0300

    Create routing.dart

---
+++ lib/core/routing.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ceiling_crm/screens/home_screen.dart';
+import 'package:ceiling_crm/screens/calculator_screen.dart';
+import 'package:ceiling_crm/screens/estimate_list_screen.dart';
+import 'package:ceiling_crm/screens/estimate_detail_screen.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+      case '/calculator':
+        return MaterialPageRoute(builder: (_) => CalculatorScreen());
+      case '/estimates':
+        return MaterialPageRoute(builder: (_) => EstimateListScreen());
+      case '/estimate_detail':
+        final estimate = settings.arguments;
+        return MaterialPageRoute(
+          builder: (_) => EstimateDetailScreen(estimate: estimate),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('Route not found: ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
