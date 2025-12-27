e73be04ccc120fbfe26f2b57ff12498e3011b001
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-27 22:15:09 +0300

    Update main.dart
    полная версия

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:ceiling_crm/app.dart';
+import 'app.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -14,18 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ceiling CRM',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
         appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          elevation: 2,
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
         ),
       ),
-      home: const App(), // Используем App как точку входа
+      home: const App(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
