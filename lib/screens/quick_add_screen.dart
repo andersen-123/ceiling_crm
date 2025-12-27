85d4c0158f6f1c7c5b7ecd6433f42fe64e69569a
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-27 22:38:24 +0300

    Update quick_add_screen.dart

--- lib/screens/quick_add_screen.dart
+++ lib/screens/quick_add_screen.dart
@@ -188,7 +188,7 @@ class _QuickAddScreenState extends State<QuickAddScreen> {
                                 children: [
                                   Text(
                                     '${item.price} ₽',
-                                    style: TextStyle(  // УБЕРИТЕ const
+                                    style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 16,
                                       color: Colors.blue[700],
