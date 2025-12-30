6336bff37a6ea91a9311ea50ca716a5500502b36
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-30 12:07:19 +0300

    Update database_helper.dart
    Исправление дублирования _database в DatabaseHelper

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -10,8 +10,6 @@ class DatabaseHelper {
 
   DatabaseHelper._init();
 
-  static Database? _database;
-
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase();
